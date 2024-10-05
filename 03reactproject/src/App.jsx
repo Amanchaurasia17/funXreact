66e8e90721ef7781c92b572e33840b5d775967de
Amanchaurasia17 <amanchaurasiya92@gmail.com>
2024-10-05 22:07:06 +0530

    adding projecct 03

---
+++ 03reactproject/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <h2>hi this is project 03 in funXreact</h2>
+   <h1>cool</h1>
+   </>
+  )
+}
+
+export default App
